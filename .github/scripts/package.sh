c6980c3791afcd2759f8e13b107e4a490812005a
nguyenvan-huan <nguyenvan.huan@kk.jp.panasonic.com>
2026-05-06 17:17:43 +0700

    Update package.sh

--- .github/scripts/package.sh
+++ .github/scripts/package.sh
@@ -7,7 +7,7 @@ PACKAGE_PATH=".github/package"   # bỏ dấu " thừa
 rm -rf "${PACKAGE_PATH}"
 mkdir -p "${PACKAGE_PATH}/${PACKAGE_NAME}"
 
-cp -r bin/Release/net8.0/linux-x64/publish/Test/ "${PACKAGE_PATH}/${PACKAGE_NAME}/"
+cp -r bin/Release/net8.0/linux-x64/publish/Test "${PACKAGE_PATH}/${PACKAGE_NAME}/"
 
 cd "${PACKAGE_PATH}"
 
