664e5ca7ed856bb8631d1dbe346ebe650bd83ead
nguyenvan-huan <nguyenvan.huan@kk.jp.panasonic.com>
2026-05-06 17:17:28 +0700

    Update package.sh

--- .github/scripts/package.sh
+++ .github/scripts/package.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-PACKAGE_NAME=DeviceControlServer
-PACKAGE_PATH=.github/package/"
+PACKAGE_NAME="DeviceControlServer"
+PACKAGE_PATH=".github/package"   # bỏ dấu " thừa
 
-rm -rf .github/package
-mkdir -p "${PACKAGE_PATH}"/"${PACKAGE_NAME}"
+rm -rf "${PACKAGE_PATH}"
+mkdir -p "${PACKAGE_PATH}/${PACKAGE_NAME}"
 
-cp -r bin/Release/net8.0/linux-x64/publish/Test/* "${PACKAGE_PATH}"/"${PACKAGE_NAME}"
+cp -r bin/Release/net8.0/linux-x64/publish/Test/ "${PACKAGE_PATH}/${PACKAGE_NAME}/"
 
-cd ${PACKAGE_PATH}
+cd "${PACKAGE_PATH}"
 
-zip -r "${PACKAGE_NAME}".zip "${PACKAGE_NAME}"
+zip -r "${PACKAGE_NAME}.zip" "${PACKAGE_NAME}"
